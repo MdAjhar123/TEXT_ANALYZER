a34a40e79091b50cb967f81928ce9a5f8517fcae
MD AJHAR <md.ajhar07860@gmail.com>
2024-01-25 23:40:04 +0530

    Add About folder, manages many bugs like zero word count, Disabled buttons, Reflex of success button etc

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = "TextUtils - Dark Mode";
+      // document.title = "TextUtils - Dark Mode";
       // setInterval(() =>{
       //   document.title = "TextUtils is Amazing Mode";
       // }, 2000);
@@ -41,7 +41,7 @@ function App() {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = "TextUtils - Light Mode";
+      // document.title = "TextUtils - Light Mode";
       
     }
   }
@@ -56,13 +56,11 @@ function App() {
     <Alert alert={alert}/>
     <div className="container my-3">
       <Routes>
-        <Route exact path="/about" element = {<About/>}/>
+        <Route exact path="/about" element = {<About mode={mode} />}/>
       </Routes>
       <Routes>
          <Route exact path="/" element = {
-          <TextForms showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-          } 
-        />
+          <TextForms showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/> } /> 
       </Routes>
     </div>
   </BrowserRouter>
