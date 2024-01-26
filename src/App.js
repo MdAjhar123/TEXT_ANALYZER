19986d70a778d8732bd2c3994813b00c1b2044b5
MD AJHAR <md.ajhar07860@gmail.com>
2024-01-26 19:26:55 +0530

    20th day

--- src/App.js
+++ src/App.js
@@ -23,8 +23,19 @@ function App() {
     }, 1500)
   }
 
+  const removeBodyClasses = () =>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+
+  }
+
   //Toggle Dark Mode
-  const toggleMode = ()=>{
+  const toggleMode = (cls)=>{
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls)
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
